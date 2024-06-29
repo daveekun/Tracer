75181c05a0b831cff6ce4566b60ba3739fdd71b3
dhorvath <dhorvath@student.hive.fi>
2024-06-29 22:19:36 +0300

    smal changes

--- include/Camera.hpp
+++ include/Camera.hpp
@@ -1,11 +1,25 @@
 #ifndef CAMERA_HPP
 #define CAMERA_HPP
 
+#include "Vector3.hpp"
+#include <vector>
+typedef struct ray
+{
+    Vector3 origin;
+    Vector3 dir;
+    bool hit;
+    Vector3 hitpoint;
+} Ray;
+
+
 class Camera
 {
     public:
-        Camera();
-        ~Camera();
+        Vector3 pos;
+        Vector3 dir;
+        float FOV;
+        std::vector<std::vector<Ray>> rays;
+        Camera(Vector3 pos = Vector3(), Vector3 dir = Vector3(1,0,0), float FOV = 60);
 };
 
 #endif
