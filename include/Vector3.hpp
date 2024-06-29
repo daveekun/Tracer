48d76763cc1fce93a05b46f3a3d2a5c98465784f
dhorvath <dhorvath@student.hive.fi>
2024-06-29 17:09:36 +0300

    simple cpp Vector3 class

--- include/Vector3.hpp
+++ include/Vector3.hpp
@@ -1,6 +1,27 @@
 #ifndef VECTOR3_HPP
 #define VECTOR3_HPP
 
+#include <stdexcept>
+#include <cmath>
+class Vector3 {
+    public:
+        float x, y, z;
+        Vector3(float _x = 0, float _y = 0, float _z = 0);
+        Vector3(const Vector3& other);
+        // operations
+        Vector3 operator+(const Vector3& other);
+        Vector3 operator-(const Vector3& other);
+        Vector3 operator*(float f);
+        Vector3 operator/(float f);
 
+        Vector3& operator=(const Vector3& other);
+
+        // comparisons
+        bool operator==(const Vector3& other);
+
+        // extra opertations
+        float length();
+        Vector3 normalised();
+};
 
 #endif
---
+++ src/Vector3.cpp
@@ -0,0 +1,44 @@
+#include "Vector3.hpp"
+
+Vector3::Vector3(float _x = 0, float _y = 0, float _z = 0) : x(_x), y(_y), z(_z) {}
+Vector3::Vector3(const Vector3& other) : x(other.x), y(other.y), z(other.z) {}
+
+Vector3 Vector3::operator+(const Vector3& other)
+{
+    return Vector3(this->x + other.x, this->y + other.y, this->z + other.z);
+}
+
+Vector3 Vector3::operator-(const Vector3& other)
+{
+    return Vector3(this->x - other.x, this->y - other.y, this->z - other.z);
+}
+
+Vector3 Vector3::operator*(float f)
+{
+    return Vector3(this->x * f, this->y * f, this->z * f);
+}
+
+Vector3 Vector3::operator/(float f)
+{
+    if (f == 0)
+        throw std::overflow_error("Zero division");
+    return Vector3(this->x / f, this->y / f, this->z / f);
+}
+
+Vector3& Vector3::operator=(const Vector3& other)
+{
+    this->x = other.x;
+    this->y = other.y;
+    this->z = other.z;
+    return (*this);
+}
+
+bool Vector3::operator==(const Vector3& other)
+{
+    return (this->x == other.x && this->y == other.y && this->z == other.z);
+}
+
+float Vector3::length()
+{
+    return (std::sqrt(x * x + y * y + z * z));
+}
